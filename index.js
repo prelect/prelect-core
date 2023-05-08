190dcffb3094b5fab60c852dd6ac6df077a0e6a7
frithsun <mattvdw@protonmail.com>
2023-05-08 03:23:14 +0000

    rewrite readme

--- index.js
+++ index.js
@@ -5,44 +5,52 @@ export default class PrelectCore {
   constructor() {
     this.prim = {
       i1: {
+        order: 0,
         type: 1n,
-        bits: 1,
+        bits: 1n,
         cast: Boolean
       },
       i8: {
+        order: 1,
         type: 2n,
-        bits: 8,
+        bits: 8n,
         cast: Uint8Array
       },
       i16: {
+        order: 2,
         type: 3n,
-        bits: 16,
+        bits: 16n,
         cast: Uint16Array
       },
       i32: {
+        order: 3,
         type: 4n,
-        bits: 32,
+        bits: 32n,
         cast: Uint32Array
       },
-      f32: {
-        type: 5n,
-        bits: 32,
-        cast: Float32Array
-      },
       i64: {
+        order: 4,
         type: 6n,
-        bits: 64,
+        bits: 64n,
         cast: BigUint64Array
       },
+      f32: {
+        order: 5,
+        type: 5n,
+        bits: 32n,
+        cast: Float32Array
+      },
       f64: {
+        order: 6,
         type: 7n,
-        bits: 64,
+        bits: 64n,
         cast: Float64Array
       },
       v128: {
+        order: 7,
         type: 8n,
-        bits: 128,
-        cast: BigInt.asUintN(128, 0n)
+        bits: 128n,
+        cast: null // BigInt.asUintN(128, ?)
       },
     };
 
@@ -70,17 +78,20 @@ export default class PrelectCore {
     this.mem[0n] = 2n; // free space pointer
     this.mem[1n] = 0n; // last table pointer
 
-    this.createTable([this.prim.i64, this.prim.f32]);
+    this.createTable(0n, 0n, [this.prim.i64, this.prim.f32]);
+    this.createTable(0n, 0n);
+    this.createTable(0n, 0n, [this.prim.i8]);
   
     console.log(this.mem.slice(0, 64));
   }
 
   // if it has a parentTable, it's an index
-  createTable(fields = [], parentTable = 0n) {
+  createTable(callingTable = 0n, parentTable = 0n, fields = []) {
     if (this.mem[0n] + 4n + BigInt(fields.length * 2) > this.mem.length) {
+      this.defrag();
+      this.createTable(fields, parentTable);
       // TODO: grow memory when overflow
       // try defrag before grow
-      throw new Error("memory overflow");
     }
 
     // TODO: handle if more than 64 bit number of fields?
@@ -95,48 +106,73 @@ export default class PrelectCore {
 
     this.mem[1n] = this.mem[0n];
 
+    // calling table pointer
+    this.mem[this.mem[1n] + 1n] = callingTable;
+
     // parent table pointer
-    this.mem[this.mem[1n] + 1n] = parentTable;
+    this.mem[this.mem[1n] + 2n] = parentTable;
 
     // data pointer
-    this.mem[this.mem[1n] + 2n] = 0n;
+    this.mem[this.mem[1n] + 3n] = 0n;
 
     // number of fields
     if (parentTable > 0) {
-      this.mem[this.mem[1n] + 3n] = this.mem[parentTable + 3n];
+      this.mem[this.mem[1n] + 4n] = this.mem[parentTable + 3n];
     } else {
-      this.mem[this.mem[1n] + 3n] = BigInt(fields.length);
+      this.mem[this.mem[1n] + 4n] = BigInt(fields.length);
     }
+
+    let rowLength = 0n;
+    fields.forEach((field) => rowLength += field.bits);
+    
+    this.mem[this.mem[1n] + 5n] = rowLength;
+
 
     fields.forEach((field, i) => {
       i = BigInt(i);
       // mutable? nullable? locked? reordered? deleted?
-      this.mem[this.mem[1n] + 4n + 2n * i + 0n] = 997n;
+      this.mem[this.mem[1n] + 6n + 2n * i + 0n] = 997n;
 
       if (parentTable > 0) {
         // order
-        this.mem[this.mem[1n] + 4n + 2n * i + 1n] = field;
+        this.mem[this.mem[1n] + 6n + 2n * i + 1n] = field;
       } else {
         // type (or formula address)
-        this.mem[this.mem[1n] + 4n + 2n * i + 1n] = field.type;
+        this.mem[this.mem[1n] + 6n + 2n * i + 1n] = field.type;
       }
     });
 
     // move cursor forward
-    this.mem[0n] = this.mem[1n] + 4n + BigInt(fields.length) * 2n;
+    this.mem[0n] = this.mem[1n] + 6n + BigInt(fields.length) * 2n;
 
     return this.mem[1n];
   }
 
-  insert(table, data) {}
+  insert(table, offset, size) {
+    if (offset + size > this.mem.length) {
+      this.defrag();
+      this.insert(table, offset, size);
+    }
+
+    this.mem[table + 2n]    
+  }
 
   update(table, row, data) {}
 
-  read(table, columns, rowFirst, rowFinal) {}
+  read(table, columns, rowFirst, rowFinal, committed = true) {
+    if (committed) this.sync();
+  }
 
   delete(table, row) {}
 
   dropTable(table) {}
+
+  sync() {}
+
+  defrag() {
+    sync();
+    throw new Error("memory overflow");
+  }
 }
 
 const prelectCore = new PrelectCore();
